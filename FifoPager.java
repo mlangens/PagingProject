6abcc2681a8dcfa8f065c28efc87b46849142890
Max Langensiepen <mlangens@emich.edu>
2013-05-24 01:17:31 -0400

    inhertinace boilerplate

---
+++ FifoPager.java
@@ -0,0 +1,19 @@
+package PagingProject;
+
+public class FifoPager extends Pager {
+
+  @Override
+  protected String pagerName() {
+    return null;
+  }
+
+  @Override
+  protected int DoPageFault() {
+    return 0;
+  }
+
+  @Override
+  protected void DoPageAccess(int frameID) {
+  }
+
+}
