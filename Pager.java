aa3b3d9544073960c6aa5573bfd43e282d0a644b
Max Langensiepen <mlangens@emich.edu>
2013-05-24 01:11:06 -0400

    maybe gonna use Pager as the driver

--- Pager.java
+++ Pager.java
@@ -1,22 +1,34 @@
 package PagingProject;
 
-class Pager {
-  int myNumFaults;
-  int myAccesses;
-  Frames myFrames;
+abstract class Pager {
+  protected int myNumFaults;
+  protected int myAccesses;
+  protected Frames myFrames;
   PrintBuffer myHistory;
-  
+
   public Pager() {
-    
+
   }
+
   public Pager(int numFrames) {
     setNumFrames(numFrames);
   }
-    
+
+  protected abstract String pagerName(); 
+  
+  protected abstract int DoPageFault();
+  
+  protected abstract void DoPageAccess(int frameID);
+  
   private void setNumFrames(int numFrames) {
-    
+
   }
-  
-  
-  
+
+  public int getMyNumFaults() {
+    return myNumFaults;
+  }
+
+  public int getMyAccesses() {
+    return myAccesses;
+  }    
 }
