9b44bc4524107abfabd09d02affc5d41943d5e0d
Max Langensiepen <mlangens@emich.edu>
2013-05-27 14:36:38 -0400

    cleanup

--- PrintBuffer.java
+++ PrintBuffer.java
@@ -48,11 +48,15 @@ public class PrintBuffer {
     for (Entry<Integer, Frame> entry : stateFulFrame.entrySet()) {
       stringBuilder[0] += entry.getKey() + space;
       ArrayList<Integer> pageElements = entry.getValue().getPageElements();
-      for (int i = 0; i < pageElements.size(); i++) {
-        String temp = stringBuilder[i + 1];
-        temp += pageElements.get(i) + space;
-        stringBuilder[i + 1] = temp;
-      }
+      printPageElements(space, pageElements);
+    }
+  }
+
+  private void printPageElements(String space, ArrayList<Integer> pageElements) {
+    for (int i = 0; i < pageElements.size(); i++) {
+      String temp = stringBuilder[i + 1];
+      temp += pageElements.get(i) + space;
+      stringBuilder[i + 1] = temp;
     }
   }
 
