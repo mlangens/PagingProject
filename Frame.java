f9360e1ab31c6937aa2152788dd80a59a9ca7792
Max Langensiepen <mlangens@emich.edu>
2013-05-27 22:05:08 -0400

    add frame object

---
+++ Frame.java
@@ -0,0 +1,38 @@
+package PagingProject;
+
+import java.util.ArrayList;
+
+public class Frame {
+  private int numFrames;
+  private ArrayList<Integer> pageElements = new ArrayList<Integer>();
+
+  public Frame() {
+    initializePageList(numFrames);
+  }
+
+  public void initializePageList(int numFrames) {
+    for (int i = 0; i < numFrames; i++) {
+      pageElements.add(0);
+    }
+  }
+
+  public int getNumFrames() {
+    return numFrames;
+  }
+
+  public boolean lookupFrameElement(int pageID) {
+    return getPageElements().contains(pageID);
+  }
+
+  public int getPageAtSlotNumber(int slotNumber) {
+    return getPageElements().get(slotNumber);
+  }
+
+  public void setNumFrames(int numFrames) {
+    this.numFrames = numFrames;
+  }
+
+  public ArrayList<Integer> getPageElements() {
+    return pageElements;
+  }
+}
