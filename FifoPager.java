b4c83dfcb6ffecaf81f385f7f3780ea3b539951a
Max Langensiepen <mlangens@emich.edu>
2013-05-27 23:15:01 -0400

    fifo queue

--- FifoPager.java
+++ FifoPager.java
@@ -1,7 +1,20 @@
 package PagingProject;
 
-public class FifoPager extends Pager {
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
 
+public class FifoPager extends Pager {
+  private Queue<Integer> fifo;
+  
+  public FifoPager(int numFrames) {
+    super(numFrames);
+    fifo = new ArrayBlockingQueue<Integer>(numFrames);
+  }
+
+  private Queue<Integer> fifoQueue = new Queue<Integer>() {
+    
   public FifoPager(int numFrames) {
     super(numFrames);
   }
